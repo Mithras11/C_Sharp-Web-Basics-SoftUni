1bf703445071a65f9969a23f7b1253500f419f8a
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-01-22 18:45:29 +0200

    updated

---
+++ BasicWebServer/BasicWebServer.Server/Responses/HtmlResponse.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BasicWebServer.Server.Responses
+{
+    using HTTP;
+
+    public class HtmlResponse : ContentResponse
+    {
+        public HtmlResponse(string text, Action<Request, Response> preRenderAction = null)
+            : base(text, ContentType.Html, preRenderAction)
+        {
+        }
+    }
+}
---
+++ BasicWebServer/BasicWebServer.Server/Responses/TextResponse.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BasicWebServer.Server.Responses
+{
+    using HTTP;
+
+    public class TextResponse : ContentResponse
+    {
+        public TextResponse(string text, Action<Request, Response> preRenderAction = null)
+            : base(text, ContentType.PlainText, preRenderAction)
+        {
+        }
+    }
+}

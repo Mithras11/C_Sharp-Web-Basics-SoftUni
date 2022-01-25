b9db920d2772f5dab69cc74be030e7901fdb93e7
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-01-25 20:56:11 +0200

    added ViewResponse

--- BasicWebServer/BasicWebServer.Server/Controllers/Controller.cs
+++ BasicWebServer/BasicWebServer.Server/Controllers/Controller.cs
@@ -3,7 +3,8 @@ namespace BasicWebServer.Server.Controllers
 {
     using Responses;
     using HTTP;
-
+    using System.Runtime.CompilerServices;
+    using System;
 
     public abstract class Controller
     {
@@ -40,5 +41,14 @@ namespace BasicWebServer.Server.Controllers
         protected Response NotFound() => new NotFoundResponse();
         protected Response Redirect(string location) => new RedirectResponse(location);
         protected Response File(string fileName) => new TextFileResponse(fileName);
+
+
+        protected Response View([CallerMemberName] string viewName = "")
+            => new ViewResponse(viewName, this.GetControllerName());
+
+
+        private string GetControllerName()
+            => this.GetType().Name
+            .Replace(nameof(Controller), string.Empty);
     }
 }
--- BasicWebServer/BasicWebServer.Server/Responses/HtmlResponse.cs
+++ BasicWebServer/BasicWebServer.Server/Responses/HtmlResponse.cs
@@ -11,3 +11,4 @@ namespace BasicWebServer.Server.Responses
         }
     }
 }
+     
